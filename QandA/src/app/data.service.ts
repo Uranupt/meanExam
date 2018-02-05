1573af3cfc5ae1700663b62514a895f8048de27d
Ross Allan <31083803+Uranupt@users.noreply.github.com>
2018-02-05 00:06:35 -0800

    Update data.service.ts

--- QandA/src/app/data.service.ts
+++ QandA/src/app/data.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'Rxjs';
+import { BehaviorSubject } from 'rxjs';
 import { Http } from '@angular/http';
 import { Router } from '@angular/router'
 
